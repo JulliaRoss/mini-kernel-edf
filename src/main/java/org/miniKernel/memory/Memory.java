4cc06569b8a1b1aaea9279384a4bd30fa52862ea
guilherme-messer <guilhermemesser@gmail.com>
2026-05-12 19:44:51 -0300

    feat: implement Memory with load/store

--- src/main/java/org/miniKernel/memory/Memory.java
+++ src/main/java/org/miniKernel/memory/Memory.java
@@ -1,4 +1,23 @@
 package org.miniKernel.memory;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Memory {
+    private final Map<String, Integer> data;
+
+    public Memory(Map<String, Integer> initial) {
+        this.data = new HashMap<>(initial);
+    }
+
+    public int load(String var) {
+        if (!data.containsKey(var)) {
+            throw new RuntimeException("Variável não definida: " + var);
+        }
+        return data.get(var);
+    }
+
+    public void store(String var, int value) {
+        data.put(var, value);
+    }
 }
